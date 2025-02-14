11806d495abcc52d507a4659c01d82fcbbdafb25
shefket-mustafa <miracle_1@abv.bg>
2025-02-14 15:43:21 +0200

    Mongoose installed
    Connection to DB established

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,6 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
+
+try{
+
+    const uri = 'mongodb://localhost:27017/furnitures';
+    await mongoose.connect(uri);
+
+    console.log('Successfully connected to DB!');
+}catch(err){
+    console.log("Connection to DB FAILED");
+    console.log(err.message);
+}
 
 app.get('/', (req,res) => {
     res.json({message: 'It works'});
